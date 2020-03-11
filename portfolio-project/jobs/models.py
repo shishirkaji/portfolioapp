51b4a58b575d8117656164d06cadbc90be345cfc
shishir <shishirkaji@gmail.com>
2020-03-11 14:33:20 +1100

    added detail pages , directed back button in detail pages, added more button to direct to detail pages and worked to access images from the database

--- portfolio-project/jobs/models.py
+++ portfolio-project/jobs/models.py
@@ -5,3 +5,6 @@ class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     #summary`
     summary = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.summary
--- portfolio-project/jobs/views.py
+++ portfolio-project/jobs/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Job
-
-def shishir(request):
-    return render(request, 'jobs/shishir.html')
 
 def home(request):
     jobs= Job.objects
     return render( request , 'jobs/home.html', {'jobs':jobs})
+
+def detail(request, job_id):
+    print(job_id)
+    job_detail = get_object_or_404(Job, pk = job_id)
+    # job_detail ='hero'
+    print(job_detail)
+    return render( request , 'jobs/detail.html' , {'job':job_detail})
